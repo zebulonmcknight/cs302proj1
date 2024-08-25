34a34b7eb8a7cf98a2929367cdde179f81a33977
zebulonmcknight <zmcknigh@vols.utk.edu>
2024-08-24 20:45:28 -0400

    quick file read test

--- lib_info.cpp
+++ lib_info.cpp
@@ -1,8 +1,31 @@
-#include <iostream> 
+// CS302 Fall 2024 Project 1: Music Library
+// Zebulon Mcknight and Semmy
+
+#include <iostream>
+#include <fstream>
+#include <string> 
 using namespace std; 
 
 int main(int argc, char *argv[])
 {
-	cout<<"Hello World!"<<endl; 
+	if (argc != 2)
+	{
+		cerr<<"usage: ./lib_info.cpp filename"<<endl; 
+		return 1; 
+	} 
+	
+	string line; 
+
+	char *filename = argv[1];
+	ifstream fin; 
+	fin.open(filename); 
+
+	while (fin>>line)
+	{
+		cout<<line<<endl; 
+	}
+
+	fin.close(); 
+
 	return 0; 
 } 
