f8d5394053e85a9b1806c15711a8496d86075f42
zebulonmcknight <zmcknigh@vols.utk.edu>
2024-08-26 11:02:26 -0400

    helper functions added

--- lib_info.cpp
+++ lib_info.cpp
@@ -4,7 +4,56 @@
 #include <iostream>
 #include <fstream>
 #include <string> 
+#include <map> 
 using namespace std; 
+
+struct Song
+{
+	string title; 
+	int time; 
+}; 
+
+struct Album
+{
+	map <int, Song> songs;
+	string name; 
+	int time; 
+	int nSongs; 
+}; 
+
+struct Artist
+{
+	map <string, Album> albums; 
+	string name; 
+	int time; 
+	int nSongs; 
+};  
+
+// Converts a string representing time of format "MM:SS" to an integer number of seconds
+int timeToInt(string formattedTime)
+{
+	int time = 0; 
+
+	string minutes = formattedTime.substr(0,2); 
+	string seconds = formattedTime.substr(3,4); 
+
+	time += stoi(minutes) * 60; 
+	time += stoi(seconds); 
+
+	return time; 	
+} 
+
+// Converts an integer representing seconds to a string in format "MM:SS"
+string timeToString(int time)
+{
+	string formattedTime; 
+
+	formattedTime.append(to_string(time / 60)); 
+	formattedTime += ':'; 
+	formattedTime.append(to_string(time % 60)); 
+
+	return formattedTime; 
+} 
 
 int main(int argc, char *argv[])
 {
@@ -12,20 +61,7 @@ int main(int argc, char *argv[])
 	{
 		cerr<<"usage: ./lib_info.cpp filename"<<endl; 
 		return 1; 
-	} 
-	
-	string line; 
-
-	char *filename = argv[1];
-	ifstream fin; 
-	fin.open(filename); 
-
-	while (fin>>line)
-	{
-		cout<<line<<endl; 
-	}
-
-	fin.close(); 
+	}  
 
 	return 0; 
 } 
